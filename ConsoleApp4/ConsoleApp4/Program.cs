624d6125fdefd153fcb961691f1be667a9569a8e
허재영 <cnkhud@naver.com>
2026-01-05 11:51:42 +0900

    커밋

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //숫자 데이터 형식: 정수와 실수를 다룰 때 사용하는 다양한 타입
+            int integerNum = 10; //정수 데이터
+            float floatNum = 3.14f; //단정밀도 실수
+            double doubleNum = 3.14159; //배정밀도 실수
+
+
+            Console.WriteLine(integerNum); //출력: 10
+            Console.WriteLine(floatNum); //출력: 3.14
+            Console.WriteLine(doubleNum); //출력 : 3.14159
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte level = 50;
+            short attack = 1500;
+            int gold = 1234567;
+            long experience = 9999999L;
+
+            Console.WriteLine($"{level}");
+            Console.WriteLine($"{attack}");
+            Console.WriteLine($"{gold:N0}");
+            Console.WriteLine($"{experience:N0}");
+        }
+    }
+}
