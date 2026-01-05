968671ac15c2adf50e6bac0d3c77777597d8dd00
허재영 <cnkhud@naver.com>
2026-01-05 10:14:21 +0900

    첫번째 커밋

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int play_score = 100;
+            double pi = 3.14;
+            char rank = 'A';
+            string player_name = "홍길동";
+
+            Console.WriteLine(play_score);
+            Console.WriteLine(pi);
+            Console.WriteLine(rank);
+            Console.WriteLine(player_name);
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string L = "Low", N = "Normal", H = "High";
+            string O = "On", F = "Off";
+
+
+            Console.WriteLine("Controls");
+            Console.WriteLine("          ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("          ┃ⓡ Camera Controls                            ┃");
+            Console.WriteLine("          ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Console.WriteLine($"          ☞ Camera Sensitivity           〈{L} 〉");
+            Console.WriteLine($"            Horizontal                 〈{N} 〉");
+            Console.WriteLine($"            Vertical                   〈{N} 〉");
+            Console.WriteLine($"            Use Motion Controls          〈{O} 〉");
+            Console.WriteLine($"            Motion Sensitivity           〈{L} 〉");
+            Console.WriteLine("          ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("          ┃ HD Rumble                                   ┃");
+            Console.WriteLine("          ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Console.WriteLine($"            Use Remble                   〈{O} 〉");
+            Console.WriteLine($"            Strength                   〈{N} 〉");
+            Console.WriteLine("          ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("          ┃ Map Controls                                ┃");
+            Console.WriteLine("          ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Console.WriteLine("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+            Console.WriteLine("           ⅡⅡ                   ⓛ Select ⓨ Reset ⓑ Back ");
+
+        }
+    }
+}
